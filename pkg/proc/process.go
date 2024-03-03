c5c3faf16d0a6a2bf28db2b60dc03ac79d9a0ecd
Camden Beard <cfbeard@ucsc.edu>
2024-03-03 13:22:28 -0800

    Parser complete.

--- pkg/proc/process.go
+++ pkg/proc/process.go
@@ -3,23 +3,23 @@ package proc
 import (
 	"fmt"
 	"os/exec"
-	"strings"
+    "strconv"
+    "strings"
 )
 
 type Proc struct {
 	uid   string
-	pid   int64
-	ppid  int64
-	c     int64
+	pid   int
+	ppid  int
+	c     int
 	stime string
 	tty   string
 	time  string
 	cmd   string
-    args []string
-	name  string
+    args  string
 }
 
-func Processes(filter ...string) ([]*Proc, error) {
+func Processes() ([]*Proc, error) {
 	processes := make([]*Proc, 0)
 
 	ps := exec.Command("ps", "-ef")
@@ -31,21 +31,25 @@ func Processes(filter ...string) ([]*Proc, error) {
 	lines := strings.Split(string(out), "\n")
 
 	for _, process := range lines {
-        //proc := &Proc{}
+        proc := &Proc{}
         var cols []string
-        args := ""
+
         process = strings.Join(strings.Fields(strings.TrimSpace(process)), " ")
-        argIndex := strings.Index(process, "-")
-        if argIndex != -1 {
-            cols = strings.Split(process[:argIndex], " ")
-            args = process[argIndex:]
-        } else {
-            cols = strings.Split(process, " ")
-        }
+
         println(process)
-        for _, col := range cols {
-          println(col)
-        }
+        cols = strings.Split(process, " ")
+
+        proc.uid = cols[0]
+        proc.pid, _ = strconv.Atoi(cols[1])
+        proc.ppid, _ = strconv.Atoi(cols[2])
+        proc.c, _ = strconv.Atoi(cols[3])
+        proc.stime = cols[4]
+        proc.tty = cols[5]
+        proc.time = cols[6]
+        proc.cmd = cols[7]
+        proc.args = strings.Join(cols[8:], " ")
+
+        processes = append(processes, proc)
 	}
 
 	return processes, nil
